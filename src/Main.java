d0a7c1c1fee03041990ccb9e2d68a39c7f776eef
adriam1991 <mgadrian456@gmail.com>
2022-03-07 10:57:02 +0100

    final

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,11 @@ public class Main {
 
         Rocket rocket1 = new Rocket("32WESSDS", 3);
         Rocket rocket2 = new Rocket("LDSFJA32", 6);
-        printResult(rocket1, rocket2);
-
+        printResult(rocket1);
+        printResult(rocket2);
     }
 
-    private static void printResult(Rocket rocket1, Rocket rocket2) {
-        System.out.println("El primer coet es: " + rocket1.getCode() + " i te " + rocket1.getPropeller() + " propulsors");
-        System.out.println("El segon coet es: " + rocket2.getCode() + " i te " + rocket2.getPropeller() + " propulsors");
+    private static void printResult(Rocket rocket) {
+        System.out.println("El coet : " + rocket.getCode() + " te " + rocket.getPropeller() + " propulsors");
     }
-
 }
